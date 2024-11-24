d9a0ca9570d33d5aa362f98e959cee85ad16e70b
kiuryd <kiritomuniz@gmail.com>
2024-11-24 10:37:37 -0400

    add

---
+++ src/main/java/net/kiuryd/modtutorial/ModTutorialDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kiuryd.modtutorial;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModTutorialDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
